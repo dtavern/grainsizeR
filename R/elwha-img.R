71dd550811e3ab092f9ffd0189732510bac76ad3
David Tavernini <david.tavernini@uleth.ca>
2017-03-04 12:03:35 -0800

    add test data and documentation

---
+++ R/elwha-img.R
@@ -0,0 +1,16 @@
+#' Example of grain sizes extracted using grainsizeR
+#'
+#' A dataset containing 100 grain size samples extracted from Elwha river imagery
+#'
+#'
+#' @format A data frame with 100 rows and 5 variables:
+#' \describe{
+#'   \item{X}{measurement number}
+#'   \item{measurement_no}{measurement number}
+#'   \item{gsize_cm}{grain size in cm}
+#'   \item{phi}{grain size in phi scale}
+#'   \item{too_small}{binary flag indicating grain size is too small to establish edge while measuring}
+#'   ...
+#' }
+#' @source measured by David Tavernini, imagery from Katie deRego
+"elwha_img"
---
+++ R/elwha-wol.R
@@ -0,0 +1,13 @@
+#' Example of grain sizes from Wolman count (sieved)
+#'
+#' A dataset containing 110 grain size samples extracted from Elwha River Wolman pebble count
+#'
+#'
+#' @format A data frame with 100 rows and 5 variables:
+#' \describe{
+#'   \item{Size.class}{sieve size}
+#'   \item{Count}{number of stones sieved}
+#'   ...
+#' }
+#' @source data from Katie deRego
+"elwha_wol"
