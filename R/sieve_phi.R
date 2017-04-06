550022b25946916d0400bdbfefb8317a922437cd
David <david.tavernini@alumni.ubc.ca>
2017-04-06 14:48:08 -0700

    fix sieve phi function

--- R/sieve_phi.R
+++ R/sieve_phi.R
@@ -11,24 +11,25 @@
 
 sieve.phi <- function(x, sieve_sizes = -log2(c(8,11,16,22.6,32,45,64,90,128,180,256,360,512,1024))){
 
-  df <- data.frame("size.phi" = numeric(0),
+  df <- data.frame("size.phi" = character(0),
                    "count" = integer(0))
-  min_size <- data.frame("size.phi" = 0,
-                         "count" = sum(x <= min(sieve_sizes)))
+  min_size <- data.frame("size.phi" = as.character(paste(">", max(sieve_sizes))),
+                         "count" = sum(x >= max(sieve_sizes)))
   df <- rbind(df, min_size)
 
   for (i in sieve_sizes){
-    if (i == max(sieve_sizes)) {
-      size <- data.frame("size.phi" = i,
-                         "count" = sum(x > i))
+    if (i == min(sieve_sizes)) {
+      size <- data.frame("size.phi" = as.character(i),
+                         "count" = sum(x < i))
       df <- rbind(df, size)
     } else {
 
-      size <- data.frame("size.phi" = i,
+      size <- data.frame("size.phi" = as.character(i),
                          "count" = sum(
-                           x > i & x <= sieve_sizes[match(i, sieve_sizes)+1]
+                           x < i & x >= sieve_sizes[match(i, sieve_sizes)+1]
                          ))
       df <- rbind(df, size)
+      df$size.phi <- as.factor(df$size.phi)
     } # end ELSE
   }# endFOR
 
